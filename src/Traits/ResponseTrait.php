bd611be27671e0753853b0aacd4f3ae2b0c000cc
jasonKonMax <jason.konmax@gmail.com>
2022-11-22 14:01:07 +0800

    新增设备激活API

--- src/Traits/ResponseTrait.php
+++ src/Traits/ResponseTrait.php
@@ -20,16 +20,46 @@ trait ResponseTrait
     }
 
     /**
+     * @param $result
+     *
+     * @return array
+     */
+    public static function successApiJson($result = [])
+    {
+        return [
+            'code'    => 0,
+            'result'  => $result,
+            'message' => 'ok',
+            'type'    => 'success'
+        ];
+    }
+
+    /**
      * @param $message
      *
      * @return array
      */
-    public static function errorJson($message="")
+    public static function errorJson($message = "")
     {
         return [
             'code'    => -1,
             'message' => $message,
             'type'    => 'error'
+        ];
+    }
+
+    /**
+     * @param $message
+     *
+     * @return array
+     */
+    public static function errorApiJson($message = "", $code = -1)
+    {
+        return [
+            'code'    => $code,
+            'message' => $message,
+            'type'    => 'error',
+            'result'  => [],
         ];
     }
 }
